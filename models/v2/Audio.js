635257d7b5f981e3286a85552b35b4f517308d1d
Yann Tagakou <tagakouyann@gmail.com>
2025-05-22 14:28:10 +0200

    fix: audio model

--- models/v2/Audio.js
+++ models/v2/Audio.js
@@ -4,8 +4,9 @@ const audioSchema = new mongoose.Schema({
   starId: { type: mongoose.Schema.Types.ObjectId, ref: 'Star', required: true },
   title: { type: String },
   description: { type: String },
-  fileUrl: { type: String, required: true },
-  sharedWith: { type: [String], default: [] },
+  key:          { type: String, required: true },   // S3 object-key
+  canView: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  canEdit: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   addedAt: { type: Date, default: Date.now },
 });
 
